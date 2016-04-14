ef67f8511383d99e245b8813dc5dd7a7d66e7dd9
mrakotomizao <mrakotomizao@gmail.com>
2016-04-14 12:39:44 +0200

    Creating Personne Controller and Model

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,7 @@
 Route::group(['middleware' => 'jwt.auth'], function () {
     Route::resource('film', 'FilmController');
     Route::resource('genre', 'GenreController');
+    Route::resource('personne', 'PersonneController');
 
     Route::get('film/getFilmWithGenre/{id}', [
         'as' => 'getFilmWithGenre',
